736b58e0355d0a57d2189ab5e7cec697a7a1d72c
Khairil Yusof <khairil.yusof@gmail.com>
2021-10-29 16:26:22 +0800

    "Add behavior: Projects"

---
+++ src/sinar/project/tests/test_behavior_projects.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from sinar.project.behaviors.projects import IProjectsMarker
+from sinar.project.testing import SINAR_PROJECT_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.behavior.interfaces import IBehavior
+from zope.component import getUtility
+
+import unittest
+
+
+class ProjectsIntegrationTest(unittest.TestCase):
+
+    layer = SINAR_PROJECT_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_behavior_projects(self):
+        behavior = getUtility(IBehavior, 'sinar.project.projects')
+        self.assertEqual(
+            behavior.marker,
+            IProjectsMarker,
+        )
